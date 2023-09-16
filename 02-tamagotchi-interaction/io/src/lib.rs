37b86f65f327fee46468eb61a13d62bcc2bbcaf3
ramandabhijana <78398153+ramandabhijana@users.noreply.github.com>
2023-09-16 19:35:20 +0800

    feat: expanded program state

--- 02-tamagotchi-interaction/io/src/lib.rs
+++ 02-tamagotchi-interaction/io/src/lib.rs
@@ -2,28 +2,38 @@
 
 use codec::{Decode, Encode};
 use gmeta::{In, InOut, Metadata};
-use gstd::prelude::*;
+use gstd::{prelude::*, ActorId};
 use scale_info::TypeInfo;
 
 #[derive(Default, Encode, Decode, TypeInfo)]
 pub struct Tamagotchi {
     pub name: String,
     pub date_of_birth: u64,
-    // TODO: 1️⃣ Add new fields
+    pub owner: ActorId,
+    pub fed: u64,
+    pub fed_block: u64,
+    pub entertained: u64,
+    pub entertained_block: u64,
+    pub slept: u64,
+    pub slept_block: u64,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum TmgAction {
     Name,
     Age,
-    // TODO: 2️⃣ Add new actions
+    Feed,
+    Entertain,
+    Sleep,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum TmgEvent {
     Name(String),
     Age(u64),
-    // TODO: 3️⃣ Add new events
+    Fed,
+    Entertained,
+    Slept,
 }
 
 pub struct ProgramMetadata;
