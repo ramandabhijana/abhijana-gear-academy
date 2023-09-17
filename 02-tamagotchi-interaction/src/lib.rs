d54db723eaad8083ad02a4d92c9062b312846881
ramandabhijana <78398153+ramandabhijana@users.noreply.github.com>
2023-09-17 11:18:01 +0800

    feat: define constants, update initialization, handle feed action

--- 02-tamagotchi-interaction/src/lib.rs
+++ 02-tamagotchi-interaction/src/lib.rs
@@ -3,18 +3,38 @@
 use gstd::{debug, exec, msg, prelude::*};
 use tmg2_io::{Tamagotchi, TmgAction, TmgEvent};
 
-// TODO: 4️⃣ Define constants
+const HUNGER_PER_BLOCK: u64 = 1;
+const BOREDOM_PER_BLOCK: u64 = 2;
+const ENERGY_PER_BLOCK: u64 = 2;
+const FILL_PER_FEED: u64 = 1000;
+const FILL_PER_ENTERTAINMENT: u64 = 1000;
+const FILL_PER_SLEEP: u64 = 1000;
+const MIN_MOOD_VALUE: u64 = 1;
+const MAX_MOOD_VALUE: u64 = 10_000;
+
 static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 
 #[no_mangle]
 extern "C" fn init() {
     let name: String = msg::load().expect("Can't decode tamagotchi name");
     let date_of_birth: u64 = exec::block_timestamp();
-    debug!(
-        "Tamagotchi was initialized with name={:?} and date_of_birth={:?}",
-        name, date_of_birth
-    );
-    unsafe { TAMAGOTCHI = Some(Tamagotchi { name, date_of_birth }) }
+    let owner = msg::source();
+    let current_block = exec::block_height();
+
+    unsafe {
+        TAMAGOTCHI = Some(Tamagotchi {
+            name,
+            date_of_birth,
+            owner,
+            fed: 1000,
+            fed_block: current_block,
+            entertained: 1000,
+            entertained_block: current_block,
+            slept: 1000,
+            slept_block: current_block,
+        })
+    }
+
     msg::reply("Successfully Initialized", 0).expect("Initialization failed");
 }
 
@@ -28,17 +48,37 @@ extern "C" fn handle() {
     };
     match action {
         TmgAction::Name => {
-            debug!("TmgAction::Name {:?}", tamagotchi.name);
             msg::reply(TmgEvent::Name(tamagotchi.name.clone()), 0)
                 .expect("Error in sending reply TmgEvent::Name");
         }
         TmgAction::Age => {
             let age = exec::block_timestamp() - tamagotchi.date_of_birth;
-            debug!("TmgAction::Age {:?}", age);
             msg::reply(TmgEvent::Age(age), 0).expect("Error in sending reply TmgEvent::Age");
         }
+        TmgAction::Feed => {
+            // To determine the number of blocks when the Tamagotchi last ate
+            let current_block = exec::block_height();
+
+            // we get to update the fed and fed_block
+
+            // we use saturating_sub so that the subtraction result won't be below 0
+            let fed = tamagotchi
+                .fed
+                .saturating_sub((current_block - tamagotchi.fed_block) * HUNGER_PER_BLOCK);
+
+            // calculate amount needed to feed
+            let fed = fed + FILL_PER_FEED;
+
+            // we make sure that amount must be in correct range
+            let fed = cmp::min(fed, MAX_MOOD_VALUE);
+            let fed = cmp::max(fed, MIN_MOOD_VALUE);
+
+            tamagotchi.fed = fed;
+            tamagotchi.fed_block = current_block;
+        }
+        TmgAction::Entertain => todo!(),
+        TmgAction::Sleep => todo!(),
     }
-    // TODO: 5️⃣ Add new logic for calculating the `fed`, `entertained` and `slept` levels
 }
 
 #[no_mangle]
